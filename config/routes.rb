f2d3a564723e6a8de910e3b41c9ac46dcfe60474
Vitalie D <vitalie.dev@gmail.com>
2017-09-22 20:00:49 +0300

    routes: root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  defaults format: :json do
+    root to: -> {[404, {}, ['']]}
+
+    scope :api do
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
