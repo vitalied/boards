25bd2469d737886ef8b32f8a4e90eb10c56c0a7b
Vitalie D <vitalie.dev@gmail.com>
2017-09-26 19:25:01 +0300

    dashboard

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,9 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
